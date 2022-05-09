2d41a5995c96f33659377022b3df2a49f49226dd
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-05-09 12:26:35 +0530

    Introduction

---
+++ IntroductiontoCSharp/IntroductiontoCSharp/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /// <summary>
+            ///Introduction to C#
+            ///Different types of comments 
+            ///1. Xml documentation comments, line comment and multiple line comment
+            /// </summary>
+            const float mass = 3.246f;             // Taking constant value, constant value doesn't change
+            float acceleration;
+            Console.WriteLine("Mass: {0}", mass); // Writing in Console
+            Console.WriteLine("Give Acceleration: ");
+            acceleration = float.Parse(Console.ReadLine()); // Reading the input from user and parsing to float 
+            Console.WriteLine("Force: {0}", mass * acceleration);
+            Console.ReadLine();
+           
+        }
+    }
+}
