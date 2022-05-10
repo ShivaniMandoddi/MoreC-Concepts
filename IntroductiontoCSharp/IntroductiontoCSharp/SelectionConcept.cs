c92adf8e6b2258a4d5823785c3e0564e055bc5d1
Shivani849 <shivani.mandoddi@xcubelabs.com>
2022-05-10 23:50:18 +0530

    If,else and Switch concept (Unit - 7)

---
+++ IntroductiontoCSharp/IntroductiontoCSharp/SelectionConcept.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntroductiontoCSharp
+{
+    class SelectionConcept
+    {
+        static void Main(string[] args)
+        {
+            float gpa = float.Parse(Console.ReadLine());
+            if (gpa >= 3.0)
+            {
+                Console.WriteLine("You made Dean's List !!");
+            }
+            else
+            {
+                Console.WriteLine("Not on Dean's List this time. Keep trying.");
+            }
+            Console.WriteLine("Enter a letter: ");
+            char musicType = char.Parse(Console.ReadLine());
+            switch (musicType)
+            {
+                case 'R':
+                    Console.WriteLine("Rock and Roll rules");
+                    break;
+                case 'B':
+                    Console.WriteLine("Blues rules");
+                    break;
+                case 'J':
+                    Console.WriteLine("Jazz rules");
+                    break;
+                case 'C':
+                    Console.WriteLine("Classical rules");
+                    break;
+                default:
+                    Console.WriteLine("You must not like music");
+                    break;
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
